460a33b3fd35e5d27ac5423738d900d56e058827
talha-mahmood <talhamahmood1261@gmail.com>
2023-04-15 21:13:15 -0700

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 'use client'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import { useState, useMemo } from 'react';
 import { getZeroDevSigner, getSocialWalletOwner } from '@zerodevapp/sdk'
+import Image from 'next/image';
+import myimg from "/public/i.png"
 
 import { 
-  SocialWallet, 
-  GoogleSocialWallet, 
-  FacebookSocialWallet,
-  GithubSocialWallet,
-  DiscordSocialWallet,
-  TwitchSocialWallet,
-  TwitterSocialWallet
+  SocialWallet
+
 } from '@zerodevapp/social-wallet';
 
 
@@ -42,18 +37,49 @@ const Home =()=>{
   const connected = !!address
 
   return (
+    
     <div>
-      {connected && 
+
+      <section className='max-w-screen-2xl mx auto px-4' >
+
+      <div className='flex flex-col md:flex-row mt-9 '>
+        {/* Left side */}
+        <div className='flex-1'>
+          
+          <h1 className='text-4xl  sm:text-5xl font-bold text-[#ffffff] '>Create Wallets with Social</h1>
+          <p className='mt-6 text-xl text-[#ffffff]'> ZeroDev makes it easy to create AA wallets, and it doesn't get easier than using social logins. This allows you to build a powerful Web3 experience where:</p>
+          <ol className='mt-1 text-xl text-[#ffffff]'>
+           <li>Your users don't need to download wallets or back up seed phrases.</li>
+           <li>You app can offer a Web2-level UX through AA features like gas-less transactions, transaction bundling, etc.</li>
+          </ol>
+          <div className='mt-5'>
+          {connected && 
         <div>
           <label>Wallet: {address}</label>
+          <br />
+          <button className='bg-teal-700 text-white px-7 py-4 rounded-full shadow-lg text-lg font-medium hover:shadow-lg hover:scale-105 duration-300' onClick={disconnect} disabled={loading}>Disconnect</button>
         </div>
       }
       <div>
-        {!connected && <button onClick={createWallet} disabled={loading}>{ loading ? 'loading...' : 'Create Wallet'}</button>}
-        {connected && 
-          <button onClick={disconnect} disabled={loading}>Disconnect</button>
-        }
+        {!connected && <button className='bg-teal-700 text-white px-7 py-4 rounded-full shadow-lg text-lg font-medium hover:shadow-lg hover:scale-105 duration-300'
+        onClick={createWallet} disabled={loading}>{ loading ? 'loading...' : 'Create Wallet'}</button>}
+        
       </div>
+          
+  </div>
+          
+   </div>
+
+        {/* Right side */}
+        <div className='flex-1'>
+          <Image src={myimg} alt="Web3" className={"border-none"}></Image>
+        </div>
+      </div>
+     
+
+    </section>
+    
+     
     </div>
   )
 }
